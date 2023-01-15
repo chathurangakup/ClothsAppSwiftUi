11e1769ec26bb450921bf5232b08953d04fd4a2a
uditha <uditha@intervest.lk>
2023-01-15 16:41:50 +0530

    Initial Commit

---
+++ ClothsApp/ClothsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClothsAppApp.swift
+//  ClothsApp
+//
+//  Created by Intervest on 2023-01-15.
+//
+
+import SwiftUI
+
+@main
+struct ClothsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
